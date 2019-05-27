b1a0bf5395c5282f3273dc0cb48bdc7f5138737a
jwin4740 <jwin4740@gmail.com>
2019-05-27 00:45:45 -0400

    any mock

--- src/test/java/com/winkle/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/winkle/unittesting/unittesting/business/ListMockTest.java
@@ -6,38 +6,41 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 
 public class ListMockTest {
 
-	List mock = mock(List.class);
+    List mock = mock(List.class);
 
-	@Test
-	public void size_basic() {
+    @Test
+    public void size_basic() {
 
-		when(mock.size()).thenReturn(5);
-		assertEquals(5, mock.size());
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
 
-	}
+    }
 
-	@Test
-	public void returnDifferentValues() {
-		when(mock.size()).thenReturn(5).thenReturn(10);
-		assertEquals(5, mock.size());
-		assertEquals(10, mock.size());
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
 
-	}
+    }
 
-	@Test
-	public void returnWithParameters(){
-		when(mock.get(0)).thenReturn("in 28 minutes");
-		assertEquals("in 28 minutes", mock.get(0));
-		assertEquals(null, mock.get(1));
-	}
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("in 28 minutes");
+        assertEquals("in 28 minutes", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+
+    @Test
+    public void returnWithGenericParameters() {
+        when(mock.get(anyInt())).thenReturn("in 28 minutes");
+        assertEquals("in 28 minutes", mock.get(0));
+        assertEquals("in 28 minutes", mock.get(1));
+    }
 
 
 }
